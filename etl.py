32a2967be964a63283946de7e81a6b2f88305868
Dylan Hrebenach <dhrebenach@underdogmedia.com>
2021-05-07 11:01:39 -0700

    Edit to hadoop version number. Upgraded from 2.7.0 -> 3.2.0. Added appName to Spark Session. Finished process_song_data func. Checked to ensure output is in correct format on AWS S3.

--- etl.py
+++ etl.py
@@ -1,6 +1,8 @@
 import configparser
 from datetime import datetime
 import os
+import findspark
+findspark.init()
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
@@ -16,32 +18,53 @@ os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 def create_spark_session():
     spark = SparkSession \
         .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .appName("Data Lake with Spark") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0") \
+        .config("spark.hadoop.fs.s3a.multipart.size", "104857600") \
         .getOrCreate()
     return spark
 
 
 def process_song_data(spark, input_data, output_data):
-    # get filepath to song data file
-    # song_data = 
+    """
+    Process song data to read data from S3 by input_data and write them back to S3 with output_data.
+    Arguments:
+        spark (spark instance) -- a spark instance
+        input_data (string) -- a filepath for input data
+        output_data (string) -- a filepath for output data
+    """
+    song_data = "{}/song_data/A/A/A/*.json".format(input_data)
     
     # read song data file
-    # df = 
+    df = spark.read.json(song_data).dropDuplicates().cache()
 
     # extract columns to create songs table
-    # songs_table = 
-    
+    songs_table = df.select(
+        col("song_id"),
+        col("title"),
+        col("artist_id"),
+        col("year"),
+        col("duration")
+    ).distinct()
+
     # write songs table to parquet files partitioned by year and artist
-    # songs_table
+    songs_table.write.partitionBy("year", "artist_id").parquet("{}songs/songs_table.parquet".format(output_data))
 
     # extract columns to create artists table
-    # artists_table = 
+    artists_table = df.select(
+        col("artist_id"),
+        col("artist_name"),
+        col("artist_location"),
+        col("artist_latitude"),
+        col("artist_longitude")
+    ).distinct() 
     
     # write artists table to parquet files
-    artists_table
-
+    artists_table.write.parquet("{}artists/artists_table.parquet".format(output_data))
 
 def process_log_data(spark, input_data, output_data):
+    print('process_log_data func has run')
+
     # get filepath to log data file
     # log_data =
 
@@ -55,21 +78,21 @@ def process_log_data(spark, input_data, output_data):
     # artists_table = 
     
     # write users table to parquet files
-    artists_table
+    # artists_table
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
+    # get_timestamp = udf()
     # df = 
     
     # create datetime column from original timestamp column
-    get_datetime = udf()
+    # get_datetime = udf()
     # df = 
     
     # extract columns to create time table
     # time_table = 
     
     # write time table to parquet files partitioned by year and month
-    time_table
+    # time_table
 
     # read in song data to use for songplays table
     # song_df = 
@@ -78,16 +101,21 @@ def process_log_data(spark, input_data, output_data):
     # songplays_table = 
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    # songplays_table
 
 
 def main():
+    print('main func has run')
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    # song_data = "s3a://udacity-dend/song_data"
+    # song_data = os.path.join(input_data, 'song_data/A/A/A/*.json')
+    log_data = "s3a://udacity-dend/log_data"
+    output_data = "s3a://data-lake-with-spark/sparkify/"
+
     
     process_song_data(spark, input_data, output_data)  
-    process_log_data(spark, input_data, output_data)
+    # process_log_data(spark, log_data, output_data)
 
 
 if __name__ == "__main__":
